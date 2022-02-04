0f32056717dd64e152875e9cb08b1097b2335d9e
InternetUnexplorer <internetunexplorer@gmail.com>
2022-02-04 02:43:45 -0800

    Add nix-channel-watcher service

---
+++ services/nix-channel-watcher.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, lib, modulesPath, ... }:
+
+let
+  repositories = [
+    "InternetUnexplorer/nix-build-service"
+    "InternetUnexplorer/nixpkgs-overlay"
+    "InternetUnexplorer/nixos-config"
+  ];
+
+  nix-channel-watcher = pkgs.fetchFromGitHub {
+    owner = "InternetUnexplorer";
+    repo = "nix-channel-watcher";
+    rev = "e903de3aed320d39322d1df42f56c8c8e5fdeca9";
+    hash = "sha256-ewD5SVD3eXzIDGCTuIMxutUOAHTQ1DoHnnCsTkVgDZg=";
+  };
+
+  nixos-unstable-hooks = pkgs.runCommandNoCC "nixos-unstable.hooks" { } ''
+    mkdir $out
+    for repo in ${toString repositories}; do
+    cat > $out/dispatch-''${repo##*/} << EOF
+    #!${pkgs.stdenv.shell}
+    set -eu
+    curl -X POST \\
+      -H "Accept: application/vnd.github.v3+json" \\
+      -H "Authorization: token \$GITHUB_TOKEN" \\
+      -d "{\"event_type\": \"Channel updated: \$1@\''${3:0:10}\", \"client_payload\": {\"channel\": \"\$1\"}}" \\
+      https://api.github.com/repos/$repo/dispatches
+    EOF
+    chmod +x $out/dispatch-''${repo##*/}
+    done
+  '';
+
+in {
+  systemd.services.nix-channel-watcher = {
+    description = "Nix channel watcher";
+
+    path = [ pkgs.curl pkgs.python3 ];
+
+    script = ''
+      cd $STATE_DIRECTORY
+
+      # Create channels.txt if it does not exist
+      [ -f channels.txt ] || cat > channels.txt << EOF
+      nixos-unstable https://nixos.org/channels/nixos-unstable none
+      EOF
+
+      # Set up the hooks
+      rm -f ./nixos-unstable.hooks
+      ln -s ${nixos-unstable-hooks} ./nixos-unstable.hooks
+
+      # Run the script
+      python3 ${nix-channel-watcher}/channel-watcher.py
+    '';
+
+    startAt = "*:0/5"; # Every 5 minutes
+
+    serviceConfig = {
+      DynamicUser = true;
+      EnvironmentFile = "/etc/github-token";
+      StateDirectory = "nix-channel-watcher";
+      TimeoutSec = 30;
+
+      PrivateDevices = true;
+      PrivateMounts = true;
+      PrivateUsers = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+    };
+  };
+}
